44c9cc7e489c01bc6008ad46adcc8d166a46985f
feng <903849123@qq.com>
2023-05-03 14:27:38 +0800

    Update: optimization module

--- mpc-opt.py
+++ mpc-opt.py
@@ -2,3 +2,16 @@
 # region import 
 
 # endregion
+
+#%% 多启动梯度下降
+def brute_force():
+    # 整数变量直接穷举
+    pass
+
+def gradient_descent():
+    # 连续变量梯度下降
+    pass
+
+def multi_start():
+    # 多次随机启动
+    pass
